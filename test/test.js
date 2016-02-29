4cb3840bc87a8b1a6a18bac83b899d20e216500c
André Cruz <andremiguelcruz@msn.com>
2016-02-29 09:33:32 +0000

    Trying nock.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,36 @@
 'use strict';
 
+const nockBack = require('nock').back;
 const expect = require('chai').expect;
 const ghIssueStats = require('../');
 
-describe('gh-issuestats', () => {
+let nockBackDone;
 
+nockBack.fixtures = `${__dirname}`;
+nockBack.setMode('record');
+nockBack('fixtures.json', (done) => { nockBackDone = done; });
+
+describe('gh-issuestats', () => {
+    after(nockBackDone);
+
+    it('should give stats of a repository with a single issues page', () => {
+        return ghIssueStats('IndigoUnited/node-gloth')
+        .then((stats) => {
+            console.log(stats);
+        });
+    });
+
+    it('should give stats of a repository with multiple issues page');
+
+    it('should use options.apiUrl');
+
+    it('should respect options.concurrency');
+
+    it('should use options.tokens when fetching pages');
+
+    it('should pass options.got to got()');
+
+    it('should pass options.tokenDealer to options.tokenDealer');
+
+    it('should identify rate limit errors correctly');
 });
